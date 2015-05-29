16d27bd33a3f66bb7866094c29d04d7e67ce3c7d
Tomer Kruvi <kruvitomer1991@gmail.com>
2015-05-29 14:33:35 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ng-infinity-scroll
-angular infinite scroll directive
+Horizontal infinite scroll directive for angular
+
+# road-map:
+1. add support for vertical scroll
+2. add demo
+3. upload to bower
+4. pretty code
+
+# inspiration
+https://a2c2a.wordpress.com/2014/04/22/how-to-do-horizontal-infinite-scrolling-in-angularjs/
---
+++ src/Infinity-scroll.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    angular.module('ng-infinity-scroll', []);
+
+    angular
+        .module('ng-infinity-scroll')
+        .directive('infinityScroll', InfinityScrollDirective);
+
+    InfinityScrollDirective.$inject = [];
+
+    /* @ngInject */
+    function InfinityScrollDirective() {
+        function link(scope, element, attrs) {
+            element.addClass('infinite-scroll');
+
+            var raw = element[0];
+
+            element.bind('scroll', function () {
+                if (scope.isLoading) {
+                    return;
+                }
+
+                if (raw.scrollLeft + raw.offsetWidth + calcDistance(scope.distance, raw.scrollWidth) >= raw.scrollWidth) {
+                    scope.$apply(scope.loadMore);
+                }
+            });
+        }
+
+        function calcDistance(distance, width) {
+            return width / distance;
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                loadMore: '&onInfinite',
+                /** distance from the edge of the infinite-scroll element to call the "on-infinite" callback, in percentage **/
+                distance: '=',
+                isLoading: '='
+            },
+            link: link
+        };
+    }
+})();
